85aa69f232d202751de5c4ff7ab709f7b769a330
Michal Boczula <michal.boczula0101@gmail.com>
2021-12-26 15:44:56 +0100

    added short circuit

--- src/Tour.js
+++ src/Tour.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Tour = ({ id, name, info, image, price, removeItem }) => {
+  const [showMore, setShowMore] = useState(false);
   return (
     <div className="cardContainer">
       <img className="cardImg" src={image} alt={name} />
-      <p className="cardTitle">{name}</p>
-      <p className="cardInfo">{info}</p>
-      <p className="cardPrice">
-        {price}${" "}
-        <button className="cardBtn" onClick={() => removeItem(id)}>
-          Not Interested
+      <div className="cardHeader">
+        <p className="cardTitle">{name}</p>
+        <p className="cardPrice">{price}$</p>
+      </div>
+      <p className="cardInfo">
+        {showMore ? info : `${info.substring(0, 200)}...`}
+        <button
+          className="cardToggleInfoBtn"
+          onClick={() => setShowMore(!showMore)}
+        >
+          {showMore ? "Show Less" : "Read More"}
         </button>
       </p>
+      <button className="cardRemoveBtn" onClick={() => removeItem(id)}>
+        Not Interested
+      </button>
     </div>
   );
 };
